a255494835943fe06fa32c08d00ddeb0fa75720b
shuyang-he <she26@wustl.edu>
2019-10-03 13:39:24 -0700

    structure change

---
+++ src/containers/restaurantFinder/restaurantFinder.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class RestaurantFinder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            restaurantList: [],
+            activeRestaurant: null,
+        };
+    }
+
+    render() {
+
+    }
+}
+
+export default RestaurantFinder;
