37100523d785f1d2d26d45746dc071a684c30815
shuyang-he <she26@wustl.edu>
2019-10-03 18:38:12 -0700

    google map api added

--- src/components/MapView/MapView.js
+++ src/components/MapView/MapView.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 
 const mapView = (props) => {
     return (
         <div>
             <h1>Map</h1>
-            <div>Map Space</div>
+            <GoogleMapReact
+                defaultCenter={{ lat: 47.444, lng: -122.176}}
+                defaultZoom={8}
+            />
         </div>
     );
 }
